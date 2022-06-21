3cd91b5b460f179f2e7f8e6920037311a951d9a0
skyriv213 <gn1007@dankook.ac.kr>
2022-06-22 01:28:42 +0900

    새로운 할인 정책 적용과 문제점/ discountPolicy 정책 변경 및 test

--- src/main/java/com/dust/hello/order/OrderServiceImpl.java
+++ src/main/java/com/dust/hello/order/OrderServiceImpl.java
@@ -2,6 +2,7 @@ package com.dust.hello.order;
 
 import com.dust.hello.discount.DiscountPolicy;
 import com.dust.hello.discount.FIxDiscountPolicy;
+import com.dust.hello.discount.RateDiscountPolicy;
 import com.dust.hello.member.Member;
 import com.dust.hello.member.MemberRepository;
 import com.dust.hello.member.MemoryMemberRepository;
@@ -9,7 +10,7 @@ import com.dust.hello.member.MemoryMemberRepository;
 public class OrderServiceImpl implements OrderService {
 
     private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private final DiscountPolicy discountPolicy = new FIxDiscountPolicy();
+    private DiscountPolicy discountPolicy ;
 
 
     @Override
