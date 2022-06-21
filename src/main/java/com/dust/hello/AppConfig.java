4e6cd5385b01e3d6f1afa6fddad419c96a42a856
kimdonghyeon <sh2115521@naver.com>
2022-06-21 21:05:23 +0900

    basic session 3

--- src/main/java/com/dust/hello/AppConfig.java
+++ src/main/java/com/dust/hello/AppConfig.java
@@ -9,21 +9,28 @@ import com.dust.hello.member.MemberServiceImpl;
 import com.dust.hello.member.MemoryMemberRepository;
 import com.dust.hello.order.OrderService;
 import com.dust.hello.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
 
+    @Bean
     public MemberService memberService(){
         return new MemberServiceImpl(memberRepository());
     }
 
-    private MemberRepository memberRepository() {
+    @Bean
+    public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public OrderService orderService(){
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
+    @Bean
     public DiscountPolicy discountPolicy() {
         return new RateDiscountPolicy();
     }
