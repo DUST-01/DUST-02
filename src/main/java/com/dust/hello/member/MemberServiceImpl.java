bcc655b84ba7a962ef77ff4cd1bcdd07d25c8928
kangdonghyeob <donghyeob.k@gmail.com>
2022-06-29 01:02:56 +0900

    add member

--- src/main/java/com/dust/hello/member/MemberServiceImpl.java
+++ src/main/java/com/dust/hello/member/MemberServiceImpl.java
@@ -3,7 +3,11 @@ package com.dust.hello.member;
 public class MemberServiceImpl implements MemberService {
 
     /*@Override 에 의해 MemberRepository 에 있는 메서드가 아닌 MemoryMemberRepository 의 메서드가 호출됨*/
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final MemberRepository memberRepository;
+
+    public MemberServiceImpl(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
 
     @Override
     public void join(Member member) {
