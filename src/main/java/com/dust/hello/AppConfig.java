7ebd447fb8b7b5251b6cbe0600ab10c2e045968d
kangdonghyeob <donghyeob.k@gmail.com>
2022-07-06 19:53:32 +0900

    add container, bean search

--- src/main/java/com/dust/hello/AppConfig.java
+++ src/main/java/com/dust/hello/AppConfig.java
@@ -9,23 +9,30 @@ import com.dust.hello.member.MemberServiceImpl;
 import com.dust.hello.member.MemoryMemberRepository;
 import com.dust.hello.order.OrderService;
 import com.dust.hello.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 // DIP 위반을 회피하기 위해 구현 객체를 생성
 // 생성한 인스턴스의 참조를 생성자를 통해 주입해준다
 // 사용 영역과 구성 영역을 분리할 수 있음
+@Configuration
 public class AppConfig {
+    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     }
 
-    private MemberRepository memberRepository() {
+    @Bean
+    public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
+    @Bean
     public DiscountPolicy discountPolicy() {
 //        return new FixDiscountPolicy();
         return new RateDiscountPolicy();
