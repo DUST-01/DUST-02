005111f40a118bf60ca575431a11b1f271603ef2
kangdonghyeob <donghyeob.k@gmail.com>
2022-07-11 21:35:34 +0900

    feat: @configuration CGLIB

---
+++ src/test/java/com/dust/hello/singleton/ConfigurationSingletonTest.java
@@ -0,0 +1,42 @@
+package com.dust.hello.singleton;
+
+import com.dust.hello.AppConfig;
+import com.dust.hello.member.MemberRepository;
+import com.dust.hello.member.MemberServiceImpl;
+import com.dust.hello.order.OrderServiceImpl;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ConfigurationSingletonTest {
+
+    @Test
+    void configurationTest() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        MemberServiceImpl memberService = ac.getBean("memberService", MemberServiceImpl.class);
+        OrderServiceImpl orderService = ac.getBean("orderService", OrderServiceImpl.class);
+        MemberRepository memberRepository = ac.getBean("memberRepository", MemberRepository.class);
+
+        MemberRepository memberRepository1 = memberService.getMemberRepository();
+        MemberRepository memberRepository2 = orderService.getMemberRepository();
+
+        System.out.println("memberRepository1 = " + memberRepository1);
+        System.out.println("memberRepository2 = " + memberRepository2);
+        System.out.println("memberRepository3 = " + memberRepository);
+
+        assertThat(memberService.getMemberRepository()).isSameAs(memberRepository);
+        assertThat(orderService.getMemberRepository()).isSameAs(memberRepository);
+    }
+
+    @Test
+    void configurationDeep() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        AppConfig bean = ac.getBean(AppConfig.class);
+
+        // class com.dust.hello.AppConfig$$EnhancerBySpringCGLIB$$5740f981
+        System.out.println("bean = " + bean.getClass());
+    }
+}
