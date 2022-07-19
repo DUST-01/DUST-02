ca4a890f44b4816cafcda51bcafe9ea2140c88cc
kangdonghyeob <donghyeob.k@gmail.com>
2022-07-19 21:50:32 +0900

    add lombok

--- src/main/java/com/dust/hello/order/OrderServiceImpl.java
+++ src/main/java/com/dust/hello/order/OrderServiceImpl.java
@@ -3,20 +3,15 @@ package com.dust.hello.order;
 import com.dust.hello.discount.DiscountPolicy;
 import com.dust.hello.member.Member;
 import com.dust.hello.member.MemberRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService {
 
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
-
-    @Autowired
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-        this.memberRepository = memberRepository;
-        this.discountPolicy = discountPolicy;
-    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
